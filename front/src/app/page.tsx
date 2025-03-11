60eacef71e4b4f2baeebff158281b1ae789135b3
RoundRonin <roundronin8@gmail.com>
2025-03-11 15:22:33 +0300

    fix(main-buttons)/add router on the main page
    Adds routing

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,15 +1,21 @@
-import Link from 'next/link'
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button"
 
 export default function Home() {
+    const router = useRouter();
+
+    const handleToOrder = () => {
+        router.push("/orders");
+    };
+
+    const handleToCreation = () => {
+        router.push("/orders/create");
+    };
+
     return (
         <div className='flex flex-col gap-2 row-start-2 items-center sm:items-stretch'>
-            <Link className='flex flex-col sm:items-stretch' href="/orders/create" passHref>
-                <Button>Create new order</Button>
-            </Link>
-            <Link className='flex flex-col sm:items-stretch' href="/orders" passHref>
-                <Button>View orders</Button>
-            </Link>
+            <Button className='flex flex-col sm:items-stretch' onClick={handleToOrder}>Create new order</Button>
+            <Button className='flex flex-col sm:items-stretch' onClick={handleToCreation}>View orders</Button>
         </div>
     );
 }
