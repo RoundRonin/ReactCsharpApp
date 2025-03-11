40858ed31e328b66aa34f2a917d999bb4b97b224
RoundRonin <roundronin8@gmail.com>
2025-03-11 15:25:57 +0300

    fix(main-buttons)/switched to SSR

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,21 +1,15 @@
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-    const router = useRouter();
-
-    const handleToOrder = () => {
-        router.push("/orders");
-    };
-
-    const handleToCreation = () => {
-        router.push("/orders/create");
-    };
-
     return (
         <div className='flex flex-col gap-2 row-start-2 items-center sm:items-stretch'>
-            <Button className='flex flex-col sm:items-stretch' onClick={handleToOrder}>Create new order</Button>
-            <Button className='flex flex-col sm:items-stretch' onClick={handleToCreation}>View orders</Button>
+            <Link className='flex flex-col sm:items-stretch' href="/orders/create" passHref>
+                <Button >Create new order</Button>
+            </Link>
+            <Link className='flex flex-col sm:items-stretch' href="/orders" passHref>
+                <Button >View orders</Button>
+            </Link>
         </div>
     );
 }
